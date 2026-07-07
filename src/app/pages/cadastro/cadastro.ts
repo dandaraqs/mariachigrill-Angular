f96105f4362d4029d4748190da308e189bc0e15e
Nubia Alexandra Rincon Perez Cardoso <nubiaalexandrarinconperez@gmail.com>
2026-07-07 20:08:21 -0300

    testeBotaoInabilitado

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Form, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+
+
+export class Cadastro 
+{
+  mostrarBotao(formCadastro : Form): boolean {
+    return Object.values(formCadastro.getControl)
+      .every(valor => String(valor ?? '').trim() !== '');
+  }
+}
